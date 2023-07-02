8cd686c19b3661086f93e63e562627ad16605a84
Nova <69091361+qzxtu@users.noreply.github.com>
2023-07-02 00:01:10 -0500

    Update Program.cs

--- AntiDe4dot/Program.cs
+++ AntiDe4dot/Program.cs
@@ -1,28 +1,134 @@
+using dnlib.DotNet;
 using System;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
-using dnlib.DotNet;
-using dnlib.DotNet.Writer;
 
 namespace AntiDe4dot
 {
     internal abstract class Program
     {
+        public static void Main(string[] args)
+        {
+            var inputFilePath = args.FirstOrDefault();
+            if (string.IsNullOrEmpty(inputFilePath))
+            {
+                Console.WriteLine("Please enter the path to the file you want to protect:");
+                inputFilePath = Console.ReadLine()?.Trim();
+
+                if (!File.Exists(inputFilePath))
+                {
+                    Console.WriteLine($"File {inputFilePath} does not exist.");
+                    return;
+                }
+            }
+
+            var isNetCore = IsNetCoreApp(inputFilePath);
+
+            var outputExtension = isNetCore ? ".dll" : ".exe";
+            var outputFilePath = Path.Combine(Path.GetDirectoryName(inputFilePath) ?? string.Empty, Path.GetFileNameWithoutExtension(inputFilePath) + "-Anti_D4dot" + outputExtension);
+
+            if (IsNetCoreApp(inputFilePath))
+            {
+                var exePath = Path.ChangeExtension(inputFilePath, ".exe");
+                if (File.Exists(exePath))
+                {
+                    var outputExePath = Path.Combine(Path.GetDirectoryName(inputFilePath) ?? string.Empty, Path.GetFileNameWithoutExtension(inputFilePath) + "-Anti_D4dot.exe");
+                    if (exePath != null) File.Copy(exePath, outputExePath, true);
+                }
+
+                var dllPath = Path.ChangeExtension(inputFilePath, ".dll");
+                inputFilePath = dllPath;
+
+            }
+
+            ModuleDefMD moduleDef;
+            try
+            {
+                moduleDef = ModuleDefMD.Load(inputFilePath);
+            }
+            catch (Exception ex)
+            {
+                PrintMessage("Error loading assembly: " + ex.Message, ConsoleColor.Red);
+                return;
+            }
+
+            var context = new Context(moduleDef);
+            const int numTypes = 100;
+            RenameTypes(context, numTypes);
+
+            var writerOptions = new dnlib.DotNet.Writer.ModuleWriterOptions(moduleDef)
+            {
+                Logger = DummyLogger.NoThrowInstance
+            };
+
+            try
+            {
+                moduleDef.Write(outputFilePath, writerOptions);
+                PrintMessage("Done. File saved as " + outputFilePath, ConsoleColor.Cyan);
+            }
+            catch (Exception ex)
+            {
+                PrintMessage("Error writing anti d4dot assembly: " + ex.Message, ConsoleColor.Red);
+            }
+
+            PrintMessage("Press any key to exit.");
+            Console.ReadKey();
+        }
+        private static bool IsNetCoreApp(string filePath)
+        {
+            if (Path.GetExtension(filePath).Equals(".exe", StringComparison.OrdinalIgnoreCase))
+            {
+                var dllPath = Path.ChangeExtension(filePath, ".dll");
+                if (File.Exists(dllPath))
+                {
+                    try
+                    {
+                        var module = ModuleDefMD.Load(dllPath);
+                        return module.GetAssemblyRefs().Any(r => r.Name == "System.Runtime");
+                    }
+                    catch (BadImageFormatException)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            try
+            {
+                var module = ModuleDefMD.Load(filePath);
+                return module.IsILOnly && IsNetCoreAssembly(module);
+            }
+            catch (BadImageFormatException)
+            {
+                return false;
+            }
+        }
+
+        private static bool IsNetCoreAssembly(ModuleDef module)
+        {
+            return module.GetAssemblyRefs().Any(r => r.Name == "System.Runtime");
+        }
+
         private static void RenameTypes(Context context, int numTypes)
         {
             using (var rng = RandomNumberGenerator.Create())
             {
                 var buffer = new byte[8];
-                foreach (var module in context.Assembly.Modules)
+                foreach (var module in context.ModuleDef.Assembly.Modules)
                 {
-                    InterfaceImpl globalItem = new InterfaceImplUser(module.GlobalType);
+                    var globalType = module.GlobalType;
+                    if (globalType == null)
+                        continue;
+
+                    var globalItem = new InterfaceImplUser(globalType);
+
                     for (var i = 0; i < numTypes; i++)
                     {
                         rng.GetBytes(buffer);
                         var typeName = $"Type_{BitConverter.ToString(buffer).Replace("-", "").Substring(0, 16)}_Qzxtu";
-                        TypeDef newTypeDef = new TypeDefUser(string.Empty, typeName, module.CorLibTypes.GetTypeRef("System", "Attribute"));
-                        InterfaceImpl newInterfaceImpl = new InterfaceImplUser(newTypeDef);
+                        var newTypeDef = new TypeDefUser(string.Empty, typeName, module.CorLibTypes.GetTypeRef("System", "Attribute"));
+                        var newInterfaceImpl = new InterfaceImplUser(newTypeDef);
                         module.Types.Add(newTypeDef);
                         newTypeDef.Interfaces.Add(newInterfaceImpl);
                         newTypeDef.Interfaces.Add(globalItem);
@@ -50,58 +156,6 @@ namespace AntiDe4dot
             }
         }
 
-        private static void Main(string[] args)
-        {
-            var inputFilePath = args.FirstOrDefault();
-            if (string.IsNullOrEmpty(inputFilePath))
-            {
-                Console.WriteLine("Please enter the path to the file you want to protect:");
-                inputFilePath = Console.ReadLine()?.Trim();
-
-                if (!File.Exists(inputFilePath))
-                {
-                    Console.WriteLine($"File {inputFilePath} does not exist.");
-                    return;
-                }
-            }
-
-            var outputFilePath = Path.Combine(Path.GetDirectoryName(inputFilePath) ?? string.Empty, Path.GetFileNameWithoutExtension(inputFilePath) + "-Anti_D4dot.exe");
-
-            AssemblyDef assemblyDef;
-            try
-            {
-                assemblyDef = AssemblyDef.Load(inputFilePath);
-            }
-            catch (Exception ex)
-            {
-                PrintMessage("Error loading assembly: " + ex.Message, ConsoleColor.Red);
-                return;
-            }
-
-            var context = new Context(assemblyDef);
-            const int numTypes = 100;
-            RenameTypes(context, numTypes);
-
-            var moduleWriterOptions = new ModuleWriterOptions(context.ManifestModule)
-            {
-                Logger = null
-            };
-
-            try
-            {
-                assemblyDef.Write(outputFilePath, moduleWriterOptions);
-                PrintMessage("Done. File saved as " + outputFilePath, ConsoleColor.Cyan);
-            }
-            catch (Exception ex)
-            {
-                PrintMessage("Error writing anti d4dot assembly: " + ex.Message, ConsoleColor.Red);
-            }
-
-            PrintMessage("Press any key to exit.");
-            Console.ReadKey();
-        }
-
-        //Message Printer Function
         private static void PrintMessage(string message, ConsoleColor color = ConsoleColor.White)
         {
             Console.ForegroundColor = color;
